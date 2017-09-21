1a0b20916a100e0364edc4848614bb876ad43b59
Rachel Zink <zink.rachel6@gmail.com>
2017-09-21 14:35:15 -0500

    Started web-caesar

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    form = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10ps;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+        <!-- create your form here -->
+        </body>
+    </html>
+    """
+
+app.run()
